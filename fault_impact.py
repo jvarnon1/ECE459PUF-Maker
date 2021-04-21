e26c851a9ba107b8b2965be01e58cea8e2a80ae5
jvarnon1 <jvarnon1@vols.utk.edu>
2021-04-21 18:04:47 -0400

    refactored for use as module, #10

--- fault_impact.py
+++ fault_impact.py
@@ -20,51 +20,54 @@ def bitsaffected(good, bad):
         n >>= 1
     return count # the number of 1 after XOR is the number of bits changed.
 
+def fault_impact(file):
+    gate_list = []
+    name_list = []
+    for l in file: #each line in the file
+        line = l.split() #split up the words on the line
+        if line[0].startswith("G"): #gates start with G
+            # Get the gate's name
+            name = line[0]
+            if name not in name_list:
+                name_list.append(name) # save name to list
+                g = gate(name) # make a new gate object
+                gate_list.append(g) # add gate object to list of gate objects
+            else: #Find the gate with that name
+                for g in gate_list:
+                    if g.name == name:
+                        break
+            
+            #Get the fault type
+            if line[1] == "/1:": # stuck at 1
+                fault_type = 1 #save stuck at 1 type
+                g.s_a_1 = g.s_a_1 + 1
+            elif line[1] == "/0:": # stuck at 0
+                fault_type = 0 #save stuck at 0 type
+                g.s_a_0 = g.s_a_0 + 1
+            
+            # Get output bits affected
+            if line[2] == "*": ## means output was affected
+                bad_output = int(line[3], 2)
+                if fault_type == 1: #for stuck at 1 faults
+                    g.o_a_1 = g.o_a_1 + bitsaffected(output,bad_output)
+                elif fault_type == 0:
+                    g.o_a_0 = g.o_a_0 + bitsaffected(output,bad_output)
+            g.get_fault_impact()
 
-gate_list = []
-name_list = []
-#Read .fault file 
+        elif line[0] == "test": #get the output of the test
+            output = int(line[len(line)-1], 2)  #the lest element in the line is the output
+        else:
+            print("Error: unrecognized file line:\n \t %s\n", l)
+    return gate_list
+
+
+#calculate Fault impact of c17
 f = open("c17.fault", "r")
-for l in f: #each line in the file
-    line = l.split() #split up the words on the line
-    if line[0].startswith("G"): #gates start with G
-        # Get the gate's name
-        name = line[0]
-        if name not in name_list:
-            name_list.append(name) # save name to list
-            g = gate(name) # make a new gate object
-            gate_list.append(g) # add gate object to list of gate objects
-        else: #Find the gate with that name
-            for g in gate_list:
-                if g.name == name:
-                    break
-        
-        #Get the fault type
-        if line[1] == "/1:": # stuck at 1
-            fault_type = 1 #save stuck at 1 type
-            g.s_a_1 = g.s_a_1 + 1
-        elif line[1] == "/0:": # stuck at 0
-            fault_type = 0 #save stuck at 0 type
-            g.s_a_0 = g.s_a_0 + 1
-        
-        # Get output bits affected
-        if line[2] == "*": ## means output was affected
-            bad_output = int(line[3], 2)
-            if fault_type == 1: #for stuck at 1 faults
-                g.o_a_1 = g.o_a_1 + bitsaffected(output,bad_output)
-            elif fault_type == 0:
-                g.o_a_0 = g.o_a_0 + bitsaffected(output,bad_output)
-        g.get_fault_impact()
-
-    elif line[0] == "test": #get the output of the test
-        output = int(line[len(line)-1], 2)  #the lest element in the line is the output
-    else:
-        print("Error: unrecognized file line:\n \t %s\n", l)
+gates = fault_impact(f)
 f.close()
-
 # print gate names with fault impact
 print("Gate: \t fault impact \n")
-for g in gate_list:
+for g in gates:
     print(g.name, g.fault_impact, "\n")
 
         
