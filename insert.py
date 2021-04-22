7b6236c3f70ad23180844499f0ea0fc854a07cf3
jvarnon1 <jvarnon1@vols.utk.edu>
2021-04-21 20:29:58 -0400

    get_file function to pull verilog and bench, #13

--- insert.py
+++ insert.py
@@ -1,9 +1,13 @@
 # insert.py
 import pandas as pd
+def get_file(circuit_name, suffix):
+    path = circuit_name + "/" + circuit_name + suffix
 def get_gate(impactdata): # impactdata is a pandas dataframe of gates sorted by fault impact
-    return impactdata.iloc[0][0]
+    for gate in impactdata["Gate"]:
+        if "->" not in gate: #ignore these reduntant gates as the wires are connected in design.
+            break
+    return gate
 
 import fault_impact
-#calculate Fault impact of c17
 gate = get_gate(fault_impact.fault_df)
 print(gate)
