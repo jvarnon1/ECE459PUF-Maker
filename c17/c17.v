ccd9729f63e4e24d5f7ee1d6d088368dd50fce70
jvarnon1 <jvarnon1@vols.utk.edu>
2021-04-24 13:05:26 -0400

    Reloaded circuit files to original, #17

--- c17/c17.v
+++ c17/c17.v
@@ -1,32 +1,14 @@
-// Verilog
-// c17
-// Ninputs 5
-// Noutputs 2
-// NtotalGates 6
-// NAND2 6
+module c17(G1,G16,G17,G2,G3,G4,G5);
+input G1,G2,G3,G4,G5;
+output G16,G17;
 
-module c17 (N1,N2,N3,N6,N7,N22,N23,K1,K2); //K4,K5); //had N22_1, N23_1
+  wire G8,G9,G12,G15;
 
-input N1,N2,N3,N6,N7,K1,K2; //K4,K5;
-
-output N22, N23; //N22_1,N23_1;
-
-wire N10,N11,N16,N19,N11_1,N23_1; // N22,N23;
-
-nand NAND2_1 (N10, N1, N3);
-nand NAND2_2 (N11, N3, N6);
-nand NAND2_3 (N16, N2, N11_1);
-nand NAND2_4 (N19, N11_1, N7);
-nand NAND2_5 (N22, N10, N16);
-nand NAND2_6 (N23_1, N16, N19);
-
-//xor xor1 (N3_1, K1, N3);
-xor xor1 (N11_1, K1, N11);
-xor xor2 (N23, K2, N23_1);
-
-//xnor xnor3 (N16_1, K3, N16);
-// xor xor4 (N22_1, K4, N22);
-// xor xor5 (N23_1, K5, N23);
-
+  nand NAND2_0(G8,G1,G3);
+  nand NAND2_1(G9,G3,G4);
+  nand NAND2_2(G12,G2,G9);
+  nand NAND2_3(G15,G9,G5);
+  nand NAND2_4(G16,G8,G12);
+  nand NAND2_5(G17,G12,G15);
 
 endmodule
