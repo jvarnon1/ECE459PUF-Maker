9754d55656f8822e49d946a17711e738fa7e75a7
jvarnon1 <jvarnon1@vols.utk.edu>
2021-04-24 13:23:25 -0400

    if else statements for differen file types, #17

--- parse_circuit.py
+++ parse_circuit.py
@@ -1,12 +1,21 @@
 import enum
 
 class circuit:
-    def __init__(self, file_type):
+    def __init__(self, f_type, file): # given the file object and the type 
         self.inputs = []
         self.outputs = []
         self.wires = []
         self.operations = []
+        
+        if f_type == file_type.bench:
+            print("Parsing bench file... \n")
+            self.parse_b(self, file)
+        elif f_type == file_type.verilog:
+            print("Parsing verilog file...\n")
+            self.parse_v(self, file)
+        else
+            print("Unrecognized circuit file\n")
 
-class file_type(enum.ENUM):
+class file_type(enum.ENUM): # .bench and .v are formatted differently, must specify when creating class
     bench = 1
     verilog = 2
